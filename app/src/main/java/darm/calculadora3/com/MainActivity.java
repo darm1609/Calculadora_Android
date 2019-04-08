a6b58eb336cda072cd079fea8ef225b22594d94a
Daniel Rodriguez <darm1609@gmail.com>
2019-04-08 09:47:28 -0500

    Agregado %

--- app/src/main/java/darm/calculadora3/com/MainActivity.java
+++ app/src/main/java/darm/calculadora3/com/MainActivity.java
@@ -29,8 +29,11 @@ public class MainActivity extends AppCompatActivity {
             display.setText("");
         if((cad.charAt(cad.length()-1)=='+' || cad.charAt(cad.length()-1)=='-' || cad.charAt(cad.length()-1)=='x' || cad.charAt(cad.length()-1)=='/') && (boton.getText().equals("+") || boton.getText().equals("-") || boton.getText().equals("x") || boton.getText().equals("/")))
         {
-            cad = cad.substring(0, cad.length()-1);
-            display.setText(cad);
+            if(cad.charAt(cad.length()-1)!='x' && cad.charAt(cad.length()-1)!='/' || !boton.getText().equals("-"))
+            {
+                cad = cad.substring(0, cad.length() - 1);
+                display.setText(cad);
+            }
         }
         cad = String.valueOf(display.getText());
         if(cad.equals("") && (boton.getText().equals("+") || boton.getText().equals("x") || boton.getText().equals("/")))
@@ -70,31 +73,42 @@ public class MainActivity extends AppCompatActivity {
             n1 = "";
             n1_aux = "";
             n2 = "";
-            for(i=cad.indexOf("x")-1;i>=0 && cad.charAt(i)!='+' && cad.charAt(i)!='-' && cad.charAt(i)!='/' && cad.charAt(i)!='x';i--)
+            boolean neg = false;
+            for (i = cad.indexOf("x") - 1; i >= 0 && cad.charAt(i) != '+' && cad.charAt(i) != '/' && cad.charAt(i) != 'x'; i--)
             {
+                if(cad.charAt(i)=='-')
+                    neg = true;
                 n1 += Character.toString(cad.charAt(i));
                 tempArrayChar = cad.toCharArray();
                 tempArrayChar[i] = 'd';
                 cad = String.valueOf(tempArrayChar);
+                if(neg)
+                    break;
             }
-            for(i=n1.length()-1;i>=0;i--)
+            neg = false;
+            for (i = n1.length() - 1; i >= 0; i--)
             {
                 n1_aux += Character.toString(n1.charAt(i));
             }
-            n1=n1_aux;
-            for(i=cad.indexOf("x")+1;i<cad.length() && cad.charAt(i)!='+' && cad.charAt(i)!='-' && cad.charAt(i)!='/' && cad.charAt(i)!='x';i++)
+            n1 = n1_aux;
+            for (i = cad.indexOf("x") + 1; i < cad.length() && cad.charAt(i) != '+' && cad.charAt(i) != '/' && cad.charAt(i) != 'x'; i++)
             {
+                if (i != cad.length() - 1)
+                    if (cad.charAt(i + 1) == '-')
+                        neg = true;
                 n2 += Character.toString(cad.charAt(i));
                 tempArrayChar = cad.toCharArray();
                 tempArrayChar[i] = 'd';
                 cad = String.valueOf(tempArrayChar);
-            }
-            cad = cad.substring(0,cad.indexOf("x")) + cad.substring(cad.indexOf("x")+1,cad.length());
-            res = Double.parseDouble(n1) * Double.parseDouble(n2);
-            for(i=cad.indexOf("d");i<cad.length();i++)
-                if(cad.charAt(i)!='d')
+                if (neg)
                     break;
-            cad = cad.substring(0,cad.indexOf("d")) + String.valueOf(res) + cad.substring(i,cad.length());
+            }
+            cad = cad.substring(0, cad.indexOf("x")) + cad.substring(cad.indexOf("x") + 1, cad.length());
+            res = Double.parseDouble(n1) * Double.parseDouble(n2);
+            for (i = cad.indexOf("d"); i < cad.length(); i++)
+                if (cad.charAt(i) != 'd')
+                    break;
+            cad = cad.substring(0, cad.indexOf("d")) + String.valueOf(res) + cad.substring(i, cad.length());
         }
         while(cad.indexOf("/")!=-1)
         {
@@ -102,24 +116,35 @@ public class MainActivity extends AppCompatActivity {
             n1 = "";
             n1_aux = "";
             n2 = "";
-            for(i=cad.indexOf("/")-1;i>=0 && cad.charAt(i)!='+' && cad.charAt(i)!='-' && cad.charAt(i)!='/' && cad.charAt(i)!='x';i--)
+            boolean neg = false;
+            for(i=cad.indexOf("/")-1;i>=0 && cad.charAt(i)!='+' && cad.charAt(i)!='/' && cad.charAt(i)!='x';i--)
             {
+                if(cad.charAt(i)=='-')
+                    neg = true;
                 n1 += Character.toString(cad.charAt(i));
                 tempArrayChar = cad.toCharArray();
                 tempArrayChar[i] = 'd';
                 cad = String.valueOf(tempArrayChar);
+                if(neg)
+                    break;
             }
+            neg = false;
             for(i=n1.length()-1;i>=0;i--)
             {
                 n1_aux += Character.toString(n1.charAt(i));
             }
             n1=n1_aux;
-            for(i=cad.indexOf("/")+1;i<cad.length() && cad.charAt(i)!='+' && cad.charAt(i)!='-' && cad.charAt(i)!='/' && cad.charAt(i)!='x';i++)
+            for(i=cad.indexOf("/")+1;i<cad.length() && cad.charAt(i)!='+' && cad.charAt(i)!='/' && cad.charAt(i)!='x';i++)
             {
+                if(i!=cad.length()-1)
+                    if(cad.charAt(i+1)=='-')
+                        neg = true;
                 n2 += Character.toString(cad.charAt(i));
                 tempArrayChar = cad.toCharArray();
                 tempArrayChar[i] = 'd';
                 cad = String.valueOf(tempArrayChar);
+                if(neg)
+                    break;
             }
             cad = cad.substring(0,cad.indexOf("/")) + cad.substring(cad.indexOf("/")+1,cad.length());
             res = Double.parseDouble(n1) / Double.parseDouble(n2);
